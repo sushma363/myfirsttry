6581f617db2812dcb3a1547b7f831cb8af3ee244
Susma Bhusal <sushmabhusal445@gmail.com>
2018-06-15 15:38:55 +0545

    clean up the commen section

--- contact.php
+++ contact.php
@@ -8,7 +8,7 @@
   <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
   <link rel="stylesheet" type="text/css" href="css/style.css" />
   
-  <!-- modernizr enables HTML5 elements and feature detects -->
+  <!--............. modernizr enables HTML5 elements and feature detects --........................................>
   <script type="text/javascript" src="js/modernizr-1.5.min.js"></script>
 </head>
 
